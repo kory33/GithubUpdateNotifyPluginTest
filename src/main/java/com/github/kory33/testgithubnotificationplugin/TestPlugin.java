75ecca9177362cb057d8c3211c4d0eae0d5c8733
Kory33 <konryoitdv@gmail.com>
2016-12-21 15:49:14 +0100

    test commit

---
+++ src/main/java/com/github/kory33/testgithubnotificationplugin/TestPlugin.java
@@ -0,0 +1,11 @@
+package com.github.kory33.testgithubnotificationplugin;
+
+import com.github.kory33.updatenotificationplugin.bukkit.github.GithubUpdateNotifyPlugin;
+
+public class TestPlugin extends GithubUpdateNotifyPlugin {
+
+	public String getGithubRepository() {
+		return "Kory33/GithubUpdateNotifyPluginTest";
+	}
+
+}
